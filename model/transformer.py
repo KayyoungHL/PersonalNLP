2a75e627cb84b6b70c805ed60505453981534bd8
KayyoungHL <kayyounghl@gmail.com>
2022-01-04 01:10:21 +0900

    how to softmax backpropagation 20220104

--- model/transformer.py
+++ model/transformer.py
@@ -5,22 +5,32 @@ class Dense():
     """
     
     """
-    def __init__(self, nodes = 1, activation = None):
+    def __init__(self, nodes = 1, activation = None, initial = "he"):
         self.nodes = nodes
-        self.activation = activation
+        self.activation = Activation(activation)
+        self.initial = initial
 
     def __initial(self):
-        self.weights = np.sqrt(np.random.rand(self.n_inputs, self.nodes)*2/self.n_inputs)
-        self.bias = np.sqrt(np.random.rand(1, self.nodes)*2/self.n_inputs)    
+        if self.initial == "he":
+            self.weights = np.sqrt(np.random.rand(self.n_inputs, self.nodes)*2/self.n_inputs)
+            self.bias = np.sqrt(np.random.rand(1, self.nodes)*2/self.n_inputs)    
 
     def call(self, input_layer):
         self.n_inputs = len(input_layer)
         self.__initial()
     
-    def feedforward(self, inputs):
-        z = inputs @ self.weights + self.bias
-        activation = Activation(self.activation)()
-        return z, activation(z)
+    def forward(self, inputs):
+        self.inputs = inputs
+        z = self.inputs @ self.weights + self.bias
+        a = self.activation()(z)
+        return a
+    
+    def backward(self, inputs):
+        da_dz = self.activation()(inputs, forward = False)
+        dz_dw = self.inputs.T @ da_dz 
+        dz_db = np.sum(da_dz)
+        dz_dx = da_dz @ self.weights.T
+        return dz_dw, dz_db, dz_dx
     
     __call__ = call
 
@@ -41,14 +51,22 @@ class Activation():
     def call(self):        
         return self.dicts[self.activation]
     
-    def none(self, x):
+    def none(self, x, forward = True):
         return x
+
+    def sigmoid(self, x, forward = True):
+        if forward:
+            self.y = 1/(1+np.exp(-x))
+            return self.y
+        else:
+            return x*(1-self.y)*self.y
     
-    def sigmoid(self, x):
-        return 1/(1+np.exp(-x))
-    
-    def softmax(self, x):
-        return np.exp(x)/np.sum(np.exp(x))   
+    def softmax(self, x, forward = True):
+        if forward:
+            self.y = np.exp(x)/np.sum(np.exp(x))
+            return self.y
+        else:
+            ...
 
     __call__ = call
 
@@ -67,7 +85,6 @@ class Transformer():
         self.d_model = d_model
         self.hs_dim = hs_dim
         
-
     def positional_encoding(self):
         self.pe = np.zeros(shape = (self.seq_len, self.embed_dim))
 
